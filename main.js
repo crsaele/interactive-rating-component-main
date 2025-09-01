224d1847994f2f99a05b9cb37ece821b78a5e02b
Carlos Al <crsaele@gmail.com>
2025-09-01 16:18:44 -0600

    Formated html and changed var for let

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-let numberContainer = document.querySelector('.number-container');
-let submitBtn = document.querySelector('#submit-btn');
-let ratingState = document.querySelector('.state');
-let completedState = document.querySelector('.completed-state');
-let rateNumber = document.querySelector('#result');
+let numberContainer = document.querySelector(".number-container");
+let submitBtn = document.querySelector("#submit-btn");
+let ratingState = document.querySelector(".state");
+let completedState = document.querySelector(".completed-state");
+let rateResult = document.querySelector("#result");
 
-numberContainer.addEventListener('click', event => {
-    var numberSelected = event.target.innerText;
-    rateNumber.innerText = numberSelected;
-    if (numberSelected != undefined){
-        submitBtn.addEventListener('click', () =>{
-            ratingState.style.display = 'none';
-            completedState.style.display = 'flex';   
-        });
-    }
-})  
+numberContainer.addEventListener("click", (event) => {
+  let numberSelected = event.target.innerText;
+  rateResult.innerText = numberSelected;
+  if (numberSelected != undefined) {
+    submitBtn.addEventListener("click", () => {
+      ratingState.style.display = "none";
+      completedState.style.display = "flex";
+    });
+  }
+});
